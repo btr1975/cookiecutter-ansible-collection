bcbfd98aa193525ecc4cd0cab7177154a8df89ac
Ben Trachtenberg <e_ben_75-python@yahoo.com>
2025-11-16 16:20:52 -0600

    added 1 tesitng item for uv

--- tests/test_cookies.py
+++ tests/test_cookies.py
@@ -1,5 +1,11 @@
-def test_bake_simple_generation(cookies, simple_generation, simple_generation_directories, simple_generation_files):
-    result = cookies.bake(extra_context=simple_generation)
+def test_bake_simple_generation_pip(
+    cookies,
+    simple_generation_pip,
+    simple_generation_directories,
+    simple_generation_files_pip,
+    generation_not_files_pip,
+):
+    result = cookies.bake(extra_context=simple_generation_pip)
 
     assert result.exit_code == 0
     assert result.exception is None
@@ -8,17 +14,43 @@ def test_bake_simple_generation(cookies, simple_generation, simple_generation_di
     for dir_name in simple_generation_directories:
         assert result.project_path.joinpath(dir_name).is_dir()
 
-    for file_name in simple_generation_files:
+    for file_name in simple_generation_files_pip:
         assert result.project_path.joinpath(file_name).is_file()
 
+    for file_name in generation_not_files_pip:
+        assert not result.project_path.joinpath(file_name).is_file()
 
-def test_bake_no_inventory_plugins(
+
+def test_bake_simple_generation_uv(
     cookies,
-    generation_no_inventory_plugins,
+    simple_generation_uv,
+    simple_generation_directories,
+    simple_generation_files_uv,
+    generation_not_files_uv,
+):
+    result = cookies.bake(extra_context=simple_generation_uv)
+
+    assert result.exit_code == 0
+    assert result.exception is None
+    assert result.project_path.name == "some_namespace.some_name"
+    assert result.project_path.is_dir()
+    for dir_name in simple_generation_directories:
+        assert result.project_path.joinpath(dir_name).is_dir()
+
+    for file_name in simple_generation_files_uv:
+        assert result.project_path.joinpath(file_name).is_file()
+
+    for file_name in generation_not_files_uv:
+        assert not result.project_path.joinpath(file_name).is_file()
+
+
+def test_bake_no_inventory_plugins_pip(
+    cookies,
+    generation_no_inventory_plugins_pip,
     generation_no_inventory_plugins_directories,
     generation_no_inventory_plugins_files,
 ):
-    result = cookies.bake(extra_context=generation_no_inventory_plugins)
+    result = cookies.bake(extra_context=generation_no_inventory_plugins_pip)
 
     assert result.exit_code == 0
     assert result.exception is None
@@ -31,13 +63,13 @@ def test_bake_no_inventory_plugins(
         assert result.project_path.joinpath(file_name).is_file()
 
 
-def test_bake_no_action_plugins(
+def test_bake_no_action_plugins_pip(
     cookies,
-    generation_no_action_plugins,
+    generation_no_action_plugins_pip,
     generation_no_action_plugins_directories,
     generation_no_action_plugins_files,
 ):
-    result = cookies.bake(extra_context=generation_no_action_plugins)
+    result = cookies.bake(extra_context=generation_no_action_plugins_pip)
 
     assert result.exit_code == 0
     assert result.exception is None
